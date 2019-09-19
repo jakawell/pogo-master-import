18aad2b56c5e8c93b11afe814bbb2fa667245a87
Jesse Kawell <jakawell@gmail.com>
2019-09-18 20:22:33 -0400

    Remove debug

--- src/services/translator.ts
+++ src/services/translator.ts
@@ -4,7 +4,6 @@ import path from 'path';
 export class TranslatorService {
   public static getTranslations(): Map<string, any> {
     if (!TranslatorService.translations) {
-      console.log(`Getting translation files from within ${path.resolve(__dirname)}`);
       const translationFiles = fs.readdirSync(path.join(__dirname, '../data/translations'));
       TranslatorService.translations = new Map<string, any>(
         translationFiles
